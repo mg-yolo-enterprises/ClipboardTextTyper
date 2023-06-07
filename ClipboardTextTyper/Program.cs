bc41525b73b7618f2f30d8268666c9c40fc0a575
Michael Goldberg <mg@yolo-enterprises.com>
2023-06-07 11:25:03 -0700

    First commit

---
+++ ClipboardTextTyper/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardTextTyper
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Typer();
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            Environment.Exit(0);
+        }
+
+        private static void Typer()
+        {
+            if (!Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                Error("Clipboard doesn't contain text!");
+                return;
+            }
+
+            var clipboardText = Clipboard.GetText(TextDataFormat.Text);
+            if (string.IsNullOrWhiteSpace(clipboardText))
+            {
+                Error("Clipboard text is blank!");
+                return;
+            }
+            if (clipboardText.Length > 500)
+            {
+                Error("Clipboard text has too many characters!");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Waiting 5 seconds");
+            for (var i = 0; i < 5; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            TypeText(clipboardText);
+            Console.WriteLine("Done");
+
+            void Error(string error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+            }
+        }
+
+        private static void TypeText(string clipboardText)
+        {
+            foreach (var character in clipboardText)
+            {
+                SendKeys.SendWait($"{{{character}}}");
+                Thread.Sleep(TimeSpan.FromMilliseconds(75));
+            }
+        }
+    }
+}
